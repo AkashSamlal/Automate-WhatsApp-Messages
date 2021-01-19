b8bd622d6139f8e3477b1b39b8b88d4dfda73929
Akash Samlal <triforcemaster345@gmail.com>
2021-01-19 15:01:42 -0500

    update

--- automate_whatsapp.py
+++ automate_whatsapp.py
@@ -17,41 +17,34 @@ webDriver.find_element_by_xpath("//*[@title='Reshanna Jagroo']").click()
 
 now = datetime.now(); 
 current_time = now.strftime("%H:%M")
-x = 4; 
+x = 3; 
 while(x > 0):
     now = datetime.now(); 
     current_time = now.strftime("%H:%M")
     print(current_time)
-    if(current_time == "09:11"):
-        webDriver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys("9:11")
-        webDriver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button/span').click()
-        x = x - 1
-        time.sleep(60)
 
+    ## If the time is 11:11 am, send '11:11' to Reshanna
     if(current_time == "11:11"):
         webDriver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys(current_time)
         webDriver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button/span').click()
         x = x - 1
         time.sleep(60)
 
+    ## If the time is 4:20, send '4:20' to Reshanna
     if(current_time == "16:20"):
         webDriver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys("4:20")
         webDriver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button/span').click()
         x = x - 1
         time.sleep(60)
-
-    if(current_time == "21:11"):
-        webDriver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys("9:11")
-        webDriver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button/span').click()
-        x = x - 1
-        time.sleep(60)
     
+    ## If the time is 11:11 pm, send '11:11' to Reshanna
     if(current_time == "23:11"):
         webDriver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys("11:11")
         webDriver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button/span').click()
         x = x - 1
         time.sleep(60)
 
+## Wait 5 seconds until program is closed
 time.sleep(5)
-
+## Close the program
 webDriver.close()
